e8025588b3f5bb8fba2aada946566b16d173bcbb
Yoni Was (yoga-lap) <yonixw@gmail.com>
2019-05-11 22:16:20 +0300

    Init

---
+++ VideoReapeater/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace VideoReapeater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axPlayer
+            // 
+            this.axPlayer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axPlayer.Enabled = true;
+            this.axPlayer.Location = new System.Drawing.Point(0, 0);
+            this.axPlayer.Name = "axPlayer";
+            this.axPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axPlayer.OcxState")));
+            this.axPlayer.Size = new System.Drawing.Size(800, 450);
+            this.axPlayer.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.axPlayer);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VideoRepeater";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axPlayer;
+    }
+}
+
---
+++ VideoReapeater/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VideoReapeater
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            axPlayer.URL = Environment.GetCommandLineArgs().Last();
+            axPlayer.settings.setMode("loop", true);
+            axPlayer.settings.volume = 100;
+            axPlayer.Ctlcontrols.play();
+        }
+    }
+}
